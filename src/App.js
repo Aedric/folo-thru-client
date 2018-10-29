8187a85491c84f698fb7f5ac944dc75c947de7e6
aedric.donovan <Aedric.Donovan@bestbuy.com>
2018-10-29 10:37:16 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          folloThru
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
